ee18bef43ec31fd42889a8effaab8547c02feb87
nikiyyy <44340403+nikiyyy@users.noreply.github.com>
2022-01-28 20:55:32 +0200

    Add files via upload

--- obuvki/spiders/Fobuvki.py
+++ obuvki/spiders/Fobuvki.py
@@ -1,3 +1,4 @@
+from gc import callbacks
 import scrapy
 from obuvki.items import ObuvkiItem
 from scrapy.loader import ItemLoader
@@ -5,7 +6,7 @@ from scrapy.loader import ItemLoader
 class FobuvkiSpider(scrapy.Spider):
     name = 'Fobuvki'
     allowed_domains = ['www.districtshoes.bg']
-    start_urls = ['https://www.districtshoes.bg/jeni-obuvki?orderBy=default&showBy=80&page=1']
+    start_urls = ['https://www.districtshoes.bg/jeni-obuvki?page=1&orderBy=default&showBy=80']
 
     def parse(self, response):
         data = response.css('div.item.preview')
@@ -19,4 +20,11 @@ class FobuvkiSpider(scrapy.Spider):
 
             yield l.load_item()
 
-        next_page
+        next_page = response.css('ul.pagination.list-unstyled').css('a::attr(href)').getall()[-1]
+        #print("https://www.districtshoes.bg" + next_page)
+        #print(response.request.url)
+        #print(type(next_page))
+        #print(type(response.request.url))
+        if next_page is not None and response.request.url != "https://www.districtshoes.bg" + next_page:
+            yield response.follow(next_page, callback = self.parse)
+        
