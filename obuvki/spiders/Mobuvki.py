a83f3f54d2578dafea426e5a8575e1ed9386f604
nikiyyy <44340403+nikiyyy@users.noreply.github.com>
2022-01-28 16:55:48 +0200

    Add files via upload

---
+++ obuvki/spiders/Mobuvki.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+class MobuvkiSpider(scrapy.Spider):
+    name = 'Mobuvki'
+    allowed_domains = ['www.districtshoes.bg']
+    start_urls = ['https://www.districtshoes.bg/muje-obuvki?orderBy=default&showBy=80&page=1']
+
+#response.css('a.title::text').getall()
+
+    def parse(self, response):
+        data = response.css('div.item.preview')
+
+        for product in data:
+            yield {
+                'brand' : product.css('a.item-brand::text').get()[1:] ,
+                'name' : product.css('a.title::text').get()[8:-1] ,
+                'price' : product.css('span.current::text').get()[:-5]
+            }
+
